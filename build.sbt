93b7abfd42c1cd35f9d87c32c47bd26de90edc85
Jordy Moos <jordymoos@gmail.com>
2016-06-20 22:19:04 +0200

    Issues with the post requests

--- build.sbt
+++ build.sbt
@@ -1,6 +1,7 @@
 
 libraryDependencies += "com.typesafe.play" %% "play-json" % "2.5.3"
 libraryDependencies += "com.twitter" %% "finagle-http" % "6.35.0"
+libraryDependencies += "org.apache.httpcomponents" % "httpclient" % "4.5.2"
 
 lazy val root = (project in file(".")).
   settings(
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,7 +5,22 @@ import com.twitter.util.{Await, Future}
 import play.api.libs.json._
 import play.api.libs.json.Reads._
 import play.api.libs.functional.syntax._
+
 import scala.collection.mutable.{Map => MutMap}
+import java.io._
+
+import org.apache.commons._
+import org.apache.http._
+import org.apache.http.client._
+import org.apache.http.client.methods.{HttpPost, HttpPut}
+import org.apache.http.impl.client.DefaultHttpClient
+import java.util.ArrayList
+
+import org.apache.http.message.BasicNameValuePair
+import org.apache.http.client.entity.UrlEncodedFormEntity
+import org.apache.http.entity.StringEntity
+import org.apache.http.util.EntityUtils
+
 
 case class State(
     alert: String,
@@ -65,16 +80,18 @@ object Main {
   def checkLights(): Unit = {
     val lights = fetchLights()
 
-    println("State:")
     println(lightState)
     val knownLights = lights.filter(lightBox => lightState.contains(lightBox.light.uniqueid)) // Only lights we know of
 
     val reachableLights = knownLights.filter(_.light.state.reachable)
+//    val reachableLights = knownLights.filter(_.light.state.on)
     val justReachableLights = reachableLights.filter(lightBox => ! lightState.get(lightBox.light.uniqueid).get.state.reachable)
+//    val justReachableLights = reachableLights.filterNot(lightBox => lightState.get(lightBox.light.uniqueid).get.state.on)
     justReachableLights.foreach(setColor)
 
     // Set the lights states
     knownLights.filter(_.light.state.bri < 250).foreach(updateState)
+//    knownLights.foreach(updateState)
 
     // Update the colors of the just reachable lights (else they would have been set to default bright by the hue)
   }
@@ -84,7 +101,6 @@ object Main {
     println(lightBox)
 
     val light = lightState(lightBox.light.uniqueid)
-
     var body = Map[String, AnyVal](
       "bri" -> light.state.bri,
       "sat" -> light.state.sat,
@@ -94,6 +110,40 @@ object Main {
     if ( ! light.state.on)
       body = body + ("on" -> true)
 
+    println("Body:")
+    println(body)
+
+    val post = new HttpPut("http://" + Config.host + ":80/api/" + Config.username + "/lights/" + lightBox.id + "/state")
+    post.setHeader("Content-type", "application/json")
+    post.setEntity(new StringEntity("""{"bri" : 20, "sat" : 144, "hue" : 14910, "on" : true}"""))
+
+    // send the post request
+    val client = new DefaultHttpClient
+    val response = client.execute(post)
+    println(EntityUtils.toString(response.getEntity))
+  }
+
+  /*
+  def setColor(lightBox: LightBox): Unit = {
+    println("Need to set light for:")
+    println(lightBox)
+
+    val light = lightState(lightBox.light.uniqueid)
+
+    var body = Map[String, AnyVal](
+      "bri" -> 20,
+//      "bri" -> light.state.bri,
+      "sat" -> light.state.sat,
+      "hue" -> light.state.hue
+    )
+
+    println("Body:")
+    println(body)
+
+    if ( ! light.state.on)
+      body = body + ("on" -> true)
+
+    println("Host: " + Config.host)
     val client: Service[http.Request, http.Response] = Http.newService(Config.host + ":80")
     val url = "/api/" + Config.username + "/lights/" + lightBox.id + "/state"
     println("Url: " + url)
@@ -110,6 +160,7 @@ object Main {
       println(rep.contentString)
     })
   }
+  */
 
   def main(args: Array[String]): Unit = {
     lightState = setupInitialState()
@@ -119,4 +170,21 @@ object Main {
       checkLights()
     }
   }
+
+/*
+  def main(args: Array[String]) {
+
+    // add name value pairs to a post object
+    val post = new HttpPut("http://192.168.2.101:80/api/p5Y67PDoNx9sQuEaUyZM9-BIePpR7lzAYt-2Q3iK/lights/1/state")
+    post.setHeader("Content-type", "application/json")
+    post.setEntity(new StringEntity("""{"bri" : 20, "sat" : 144, "hue" : 14910, "on" : true}"""))
+
+    // send the post request
+    val client = new DefaultHttpClient
+    val response = client.execute(post)
+    println("--- HEADERS ---")
+    response.getAllHeaders.foreach(arg => println(arg))
+    println(EntityUtils.toString(response.getEntity))
+  }
+  */
 }
